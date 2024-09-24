6c76d90ade3cc12757986200a373f45a8bf12697
alighazali13 <alighazalizadeh13@gmail.com>
2024-09-24 12:57:12 +0330

    add kiosk_detailes template and change img addr to django template img

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("kiosk/", views.kiosk, name="kiosk")
+    path("kiosk/", views.kiosk, name="kiosk"),
+    path("kiosk/<str:meta_title>", views.kiosk_detailes, name="kiosk_detailes"),
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 def kiosk(request):
 
     return render(request, 'kiosk/kiosk.html')
+
+def kiosk_detailes(request, meta_title):
+
+    return render(request, 'kiosk/kiosk_detailes.html')
