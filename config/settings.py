6867a2bd23078a62810790b14f849a3418b8fa5f
alighazali13 <alighazalizadeh13@gmail.com>
2024-10-16 21:16:43 +0330

    debug

--- config/settings.py
+++ config/settings.py
@@ -102,7 +102,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'databases/db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
